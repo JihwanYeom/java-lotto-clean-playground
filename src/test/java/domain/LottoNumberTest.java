926f65e59129663ea1e362523b0606260783e342
jihwanyeom <yum020428@gmail.com>
2025-04-06 03:04:00 +0900

    refactor : Parameterized Test 적용

--- src/test/java/domain/LottoNumberTest.java
+++ src/test/java/domain/LottoNumberTest.java
@@ -5,48 +5,31 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class LottoNumberTest {
 
-    @Test
-    @DisplayName("로또 번호가 1보다 작으면 예외가 발생해야 한다")
-    void testLottoNumberSmallerThanMinimumException() {
-        int invalidNumber = 0;
-
-        assertThatThrownBy(() -> LottoNumber.from(invalidNumber))
+    @ParameterizedTest
+    @ValueSource(ints = {0, 46})
+    @DisplayName("로또 번호가 1보다 작거나 45보다 크면 예외가 발생해야 한다")
+    void testLottoNumberOutOfRangeException(int number) {
+        assertThatThrownBy(() -> LottoNumber.from(number))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("로또 번호는 1~45 사이의 값이어야 합니다.");
     }
 
-    @Test
-    @DisplayName("로또 번호가 45보다 크면 예외가 발생해야 한다")
-    void testLottoNumberBiggerThanMaximumException() {
-        int invalidNumber = 46;
+    @ParameterizedTest
+    @CsvSource({
+            "1, 1, true",
+            "1, 2, false"
+    })
+    @DisplayName("로또 번호가 같으면 참, 다르면 거짓이 반환되어야 한다")
+    void testLottoNumberIsEquals(int number1, int number2, boolean expected) {
+        LottoNumber testNumber = LottoNumber.from(number1);
 
-        assertThatThrownBy(() -> LottoNumber.from(invalidNumber))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("로또 번호는 1~45 사이의 값이어야 합니다.");
-    }
-
-    @Test
-    @DisplayName("로또 번호 1이 로또 번호 1과 같은지 비교하면 참이 반환되어야 한다")
-    void testLottoNumberIsEquals() {
-        LottoNumber testNumber = LottoNumber.from(1);
-        boolean expected = true;
-
-        boolean actual = testNumber.equals(LottoNumber.from(1));
-
-        assertThat(actual).isEqualTo(expected);
-    }
-
-    @Test
-    @DisplayName("로또 번호 2가 로또 번호 1과 같은지 비교하면 거짓이 반환되어야 한다")
-    void testLottoNumberIsNotEquals() {
-
-        LottoNumber testNumber = LottoNumber.from(2);
-        boolean expected = false;
-
-        boolean actual = testNumber.equals(LottoNumber.from(1));
+        boolean actual = testNumber.equals(LottoNumber.from(number2));
 
         assertThat(actual).isEqualTo(expected);
     }
--- src/test/java/domain/LottoRankTest.java
+++ src/test/java/domain/LottoRankTest.java
@@ -2,18 +2,31 @@ package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class LottoRankTest {
 
-    @Test
+    @ParameterizedTest(name = "당첨 번호 개수 : {0}, 보너스 번호 당첨 : {1}, 순위 : {2}")
+    @MethodSource("arguments")
     @DisplayName("일치한 당첨 번호 개수와 보너스 번호 일치 여부에 따라 알맞게 순위를 매겨야 한다.")
-    void testLottoRankMatch() {
-        assertThat(LottoRank.matchRank(6, false)).isEqualTo(LottoRank.FIRST_PRIZE);
-        assertThat(LottoRank.matchRank(5, true)).isEqualTo(LottoRank.SECOND_PRIZE);
-        assertThat(LottoRank.matchRank(3, false)).isEqualTo(LottoRank.FIFTH_PRIZE);
-        assertThat(LottoRank.matchRank(2, false)).isEqualTo(LottoRank.NO_PRIZE);
+    void testLottoRankMatch(int matchCount, boolean matchBonusNumber, LottoRank rank) {
+        assertThat(LottoRank.matchRank(matchCount, matchBonusNumber)).isEqualTo(rank);
+    }
+
+    static Stream<Arguments> arguments() {
+        return Stream.of(
+                Arguments.arguments(6,false, LottoRank.FIRST_PRIZE),
+                Arguments.arguments(5,true, LottoRank.SECOND_PRIZE),
+                Arguments.arguments(5,false, LottoRank.THIRD_PRIZE),
+                Arguments.arguments(4,false, LottoRank.FOURTH_PRIZE),
+                Arguments.arguments(3,false, LottoRank.FIFTH_PRIZE),
+                Arguments.arguments(2,false, LottoRank.NO_PRIZE)
+        );
     }
 
 }
